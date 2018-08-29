85b30a110526f03e1ab7ec7751ae9468f7eedb33
ivan <ivan@stonejungle.org>
2018-08-29 14:46:10 +0700

    Lab6 version 1

--- Lab6/Server/src/main/java/com/company/Devices/ICrudAction.java
+++
@@ -1,8 +0,0 @@
-package com.company.Devices;
-
-interface ICrudAction {
-    void create();
-    void read();
-    void update(String m_count, String m_name, String m_price, String m_company, String m_model, String m_os, String m_arg, String m_arg2);
-    void delete();
-}
---
+++ Lab6/src/main/java/com/company/Server.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import com.company.Check.*;
+import java.net.*;
+
+
+public class Server {
+    public static void main (String args[]) {
+
+        Orders ServerOrders = new Orders();
+        Validate ServerVal = new Validate(5000, 5, ServerOrders, 7005, 7020, "127.0.0.1");
+        Thread thr = new Thread(ServerVal);
+        thr.start();
+
+        int port = 7020;
+
+        // Создаем сокет для класса проверки статуса и пересылаем сигнал готовности клиенту
+        ServerVal.CreateSocket();
+
+        //Проверим доступность порта
+        //Создание TCP клиента
+        Socket clientSocket = new Socket();
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            clientSocket.setSoTimeout(1000);
+            clientSocket = serverSocket.accept();
+        } catch (Exception e) {
+            System.out.println("Ошибка cоздания TCP " + e);
+            System.exit(-1);
+        }
+
+        TCPreading ClientReader = new TCPreading(clientSocket, ServerOrders);
+
+        //Начать чтение клиента
+        Thread thr1 = new Thread(ClientReader);
+        thr1.start();
+
+    }
+}
