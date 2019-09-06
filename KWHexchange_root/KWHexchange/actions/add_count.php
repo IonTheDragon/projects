2685536045b9e6bae1e03e4a9e9aa821d131d001
IonTheDragon <50141105+IonTheDragon@users.noreply.github.com>
2019-09-06 19:02:25 +0700

    Update add_count.php
    Handled <= 0 exception

--- KWHexchange_root/KWHexchange/actions/add_count.php
+++ KWHexchange_root/KWHexchange/actions/add_count.php
@@ -9,22 +9,27 @@ include $_SERVER['DOCUMENT_ROOT'].'/KWHexchange/config.php';
 if (isset($_SESSION['login'])){
 	if(isset($_POST['count'])) {
 		$count = $_POST['count'];
-		$user = new User();
-		$database = new Database($sql_user,$sql_host,$sql_password,$sql_database,'stock_kwh');
-		$status = $user->load($database,$_SESSION['login']['account_id']);
-		if($status){
-			$fill_status = $user->fill($database,$count);
-			if($fill_status) {
-				$log = 'Added '.$count.' '.$_SESSION['login']['currency'].' at '.date("Y-m-d H:i:s").PHP_EOL;
-				$_SESSION['info'] = $log;
-				$file = $_SERVER['DOCUMENT_ROOT'].'/KWHexchange/logs/'.$_SESSION['login']['account_id'].'.log';
-				file_put_contents($file, $log, FILE_APPEND);					
-			}
-			else {
-				$_SESSION['error'] = 'Transfer error';
-			}
+		if($count<=0) {
+			$_SESSION['error'] = 'Transfer error';	
 		}
-		else $_SESSION['error'] = 'No data found';
+		else {
+			$user = new User();
+			$database = new Database($sql_user,$sql_host,$sql_password,$sql_database,'stock_kwh');
+			$status = $user->load($database,$_SESSION['login']['account_id']);
+			if($status){
+				$fill_status = $user->fill($database,$count);
+				if($fill_status) {
+					$log = 'Added '.$count.' '.$_SESSION['login']['currency'].' at '.date("Y-m-d H:i:s").PHP_EOL;
+					$_SESSION['info'] = $log;
+					$file = $_SERVER['DOCUMENT_ROOT'].'/KWHexchange/logs/'.$_SESSION['login']['account_id'].'.log';
+					file_put_contents($file, $log, FILE_APPEND);					
+				}
+				else {
+					$_SESSION['error'] = 'Transfer error';
+				}
+			}
+			else $_SESSION['error'] = 'No data found';
+		}
 	}
 	else $_SESSION['error'] = 'No input data'; 
 }
