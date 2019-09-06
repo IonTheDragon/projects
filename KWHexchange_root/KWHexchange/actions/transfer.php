621f869fea2cacbd99578f4556ee7b400f3cea96
IonTheDragon <50141105+IonTheDragon@users.noreply.github.com>
2019-09-06 19:03:44 +0700

    Update transfer.php
    Added <= 0 exception

--- KWHexchange_root/KWHexchange/actions/transfer.php
+++ KWHexchange_root/KWHexchange/actions/transfer.php
@@ -9,26 +9,31 @@ include $_SERVER['DOCUMENT_ROOT'].'/KWHexchange/config.php';
 if (isset($_SESSION['login'])){
 	if(isset($_POST['count']) && isset($_POST['account'])) {
 		$count = $_POST['count'];
-		$account = $_POST['account'];
-		$user = new User();
-		$database = new Database($sql_user,$sql_host,$sql_password,$sql_database,'stock_kwh');
-		$status = $user->load($database,$_SESSION['login']['account_id']);
-		if($status){
-			$transfer_status = $user->transfer($database,$account,$count);
-			if($transfer_status) {
-				$log_from = 'Sent '.$count.' '.$_SESSION['login']['currency'].' to '.$account.' at '.date("Y-m-d H:i:s").PHP_EOL;
-				$log_to = 'Received '.$count.' '.$_SESSION['login']['currency'].' from '.$_SESSION['login']['account_id'].' at '.date("Y-m-d H:i:s").PHP_EOL;
-				$_SESSION['info'] = $log_from;
-				$file_from = $_SERVER['DOCUMENT_ROOT'].'/KWHexchange/logs/'.$_SESSION['login']['account_id'].'.log';
-				$file_to = $_SERVER['DOCUMENT_ROOT'].'/KWHexchange/logs/'.$account.'.log';
-				file_put_contents($file_from, $log_from, FILE_APPEND);
-				file_put_contents($file_to, $log_to, FILE_APPEND);					
-			}
-			else {
-				$_SESSION['error'] = 'Transfer error';
-			}
+		if($count<=0) {
+			$_SESSION['error'] = 'Transfer error';	
 		}
-		else $_SESSION['error'] = 'No data found';
+		else {
+			$account = $_POST['account'];
+			$user = new User();
+			$database = new Database($sql_user,$sql_host,$sql_password,$sql_database,'stock_kwh');
+			$status = $user->load($database,$_SESSION['login']['account_id']);
+			if($status){
+				$transfer_status = $user->transfer($database,$account,$count);
+				if($transfer_status) {
+					$log_from = 'Sent '.$count.' '.$_SESSION['login']['currency'].' to '.$account.' at '.date("Y-m-d H:i:s").PHP_EOL;
+					$log_to = 'Received '.$count.' '.$_SESSION['login']['currency'].' from '.$_SESSION['login']['account_id'].' at '.date("Y-m-d H:i:s").PHP_EOL;
+					$_SESSION['info'] = $log_from;
+					$file_from = $_SERVER['DOCUMENT_ROOT'].'/KWHexchange/logs/'.$_SESSION['login']['account_id'].'.log';
+					$file_to = $_SERVER['DOCUMENT_ROOT'].'/KWHexchange/logs/'.$account.'.log';
+					file_put_contents($file_from, $log_from, FILE_APPEND);
+					file_put_contents($file_to, $log_to, FILE_APPEND);					
+				}
+				else {
+					$_SESSION['error'] = 'Transfer error';
+				}
+			}
+			else $_SESSION['error'] = 'No data found';
+		}
 	}
 	else $_SESSION['error'] = 'No input data'; 
 }
